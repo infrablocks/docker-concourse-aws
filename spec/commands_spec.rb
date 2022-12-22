56154ba91f44055a0b79ab1b4a1acf8493e5e588
Toby <tobyclemson@gmail.com>
2022-12-22 23:53:26 +0000

    Bump concourse version. Bump ubuntu version.

--- spec/commands_spec.rb
+++ spec/commands_spec.rb
@@ -18,7 +18,7 @@ describe 'commands' do
 
   it 'includes the concourse command' do
     expect(command('/opt/concourse/bin/concourse --version').stdout)
-      .to(match(/7\.8/))
+      .to(match(/7\.9/))
   end
 
   %w[bash curl dumb-init].each do |apk|
